31077b53930bc71f66843f848e49d93bf38f6edc
Michael Holtzscher <mholtz@protonmail.com>
2018-11-14 12:30:18 -0600

    Add docstrings to appease pylint.

--- cleanser/__init__.py
+++ cleanser/__init__.py
@@ -1,3 +1,4 @@
+"""Utilities for cleaning text."""
 import re
 
 REDDIT_MENTIONS = re.compile(r"/?u/\S+")
@@ -6,16 +7,20 @@ WHITESPACE = re.compile(r"\s\s+")
 
 
 def reddit_mentions(text: str) -> str:
+    """Removes reddit user mentions from text."""
     return REDDIT_MENTIONS.sub("", text)
 
 
 def reddit_subreddits(text: str) -> str:
+    """Removes reddit user mentions from text."""
     return REDDIT_SUBREDDITS.sub("", text)
 
 
 def urls(text: str) -> str:
-    return "None"
+    """Removes urls from text."""
+    return text
 
 
 def whitespace(text: str) -> str:
+    """Removes extra spaces, tabs, and newlines from text."""
     return WHITESPACE.sub(" ", text).strip()
